06ea6bd4428e154d96c457f4c0630551dcd19c2e
Vaibhavi-Apps <vppanchalofficial@gmail.com>
2021-06-17 11:39:51 +0530

    Initial Commit

--- app/src/main/java/com/vppanchaofficial/progresstimer/MainActivity.kt
+++ app/src/main/java/com/vppanchaofficial/progresstimer/MainActivity.kt
@@ -1,10 +1,12 @@
 package com.vppanchaofficial.progresstimer
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
 import android.widget.Button
 import android.widget.ProgressBar
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -18,14 +20,45 @@ class MainActivity : AppCompatActivity() {
         var progress_value = findViewById<TextView>(R.id.progress_value);
 
         val SHOW_PROGRESS = findViewById<Button>(R.id.SHOW_PROGRESS).setOnClickListener {
-            if(counter <= 90) {
+
+            //This is for increase progress by 10%
+
+            /*if(counter <= 90) {
                 counter += 10;
                 progress_value.text = counter.toString() + " %"
                 progress_circular.setProgress(counter);
             }else{
                 progress_value.text = "Progress Completed"
-            }
-        }
+            }*/
 
+
+            //Code for increase progress with time
+
+            val twoMin = 1 * 60 * 1000 // minutes in milli seconds
+            progress_circular.max = twoMin / 1000
+
+            object : CountDownTimer(twoMin.toLong(), 1000) {
+                override fun onTick(millisUntilFinished: Long) {
+                    val seconds: Long = millisUntilFinished / 1000
+
+                    //var seconds = (millisUntilFinished /1000).toInt()
+                    //progress_value.text =  Math.round(millisUntilFinished * 0.001f).toString()
+                    //val barVal: Int = progress_circular.max - ((seconds / 60 * 100).toInt() + (seconds % 60).toInt())
+                    //progress_circular.progress = barVal
+
+                    progress_value.text =  String.format(
+                        "%02d",
+                        seconds / 60
+                    ) + ":" + String.format("%02d", seconds % 60)
+                    progress_circular.progress = Math.round(millisUntilFinished * 0.001f)
+
+                }
+
+                override fun onFinish() {
+                    // DO something when minutes is up
+                    progress_circular.progress = 0
+                }
+            }.start()
+        }
     }
 }
